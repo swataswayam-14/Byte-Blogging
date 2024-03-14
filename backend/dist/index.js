d96cedd2ebee35c321f6cacae6693438955ad9bb
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-03-15 04:16:29 +0530

    backend routes completed

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -54,6 +54,184 @@ app.post('/api/v1/signup', (req, res) => __awaiter(void 0, void 0, void 0, funct
         });
     }
 }));
+const signInBody = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string()
+});
+app.post('/api/v1/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { success } = signInBody.safeParse(req.body);
+    if (!success) {
+        return res.status(411).json({
+            msg: 'Wrong inputs'
+        });
+    }
+    try {
+        const user = yield prisma.user.findUnique({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (!user) {
+            return res.status(411).json({
+                msg: 'User does not exists'
+            });
+        }
+        const token = jsonwebtoken_1.default.sign({ userId: user.id }, 'babablacksheephavewool', { expiresIn: '1h' });
+        return res.json({
+            msg: `User with email: ${user.email} is logged in.`,
+            token: token
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            msg: 'Server Down'
+        });
+    }
+}));
+const blogBody = zod_1.z.object({
+    title: zod_1.z.string(),
+    content: zod_1.z.string()
+});
+app.post('/blog', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, 'babablacksheephavewool');
+        console.log('Decoded token:', decoded);
+        const { success } = blogBody.safeParse(req.body);
+        if (!success) {
+            return res.status(411).json({
+                msg: 'Wrong inputs'
+            });
+        }
+        const post = yield prisma.post.create({
+            data: {
+                title: req.body.title,
+                content: req.body.content,
+                authorId: decoded.userId
+            }
+        });
+        if (!post) {
+            return res.status(500).json({
+                msg: 'Server Down'
+            });
+        }
+        return res.status(200).json({
+            msg: `blog post created having the title ${post.title}`
+        });
+    }
+    catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+}));
+//route to get a specific blog post
+app.get('/blog/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _b;
+    const token = (_b = req.headers.authorization) === null || _b === void 0 ? void 0 : _b.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, 'babablacksheephavewool');
+        console.log('Decoded token:', decoded);
+        const postId = req.params.id;
+        const post = yield prisma.post.findUnique({
+            where: {
+                id: postId
+            }
+        });
+        if (!post) {
+            return res.status(500).json({
+                msg: 'Server Down'
+            });
+        }
+        return res.json({
+            title: post.title,
+            content: post.content
+        });
+    }
+    catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+}));
+//route to get all the post that a user has
+app.get('/blogposts', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _c;
+    const token = (_c = req.headers.authorization) === null || _c === void 0 ? void 0 : _c.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, 'babablacksheephavewool');
+        console.log('Decoded token:', decoded);
+        const posts = yield prisma.post.findMany({
+            where: {
+                authorId: decoded.userId
+            }
+        });
+        if (!posts) {
+            return res.status(500).json({
+                msg: 'Server Down'
+            });
+        }
+        return res.json({
+            posts: posts
+        });
+    }
+    catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+}));
+//route to update a blog post
+app.put('/blogupdate', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _d;
+    const token = (_d = req.headers.authorization) === null || _d === void 0 ? void 0 : _d.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, 'babablacksheephavewool');
+        console.log('Decoded token:', decoded);
+        const title = req.body.title;
+        const content = req.body.content;
+        const id = req.body.id;
+        const updatedPost = yield prisma.post.update({
+            where: {
+                id: id,
+                authorId: decoded.userId
+            },
+            data: {
+                title: title,
+                content: content
+            }
+        });
+        return res.json({
+            updatedPost
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            msg: 'Server Down'
+        });
+    }
+}));
 app.listen(3000, () => {
-    console.log('working');
+    console.log('server is listening on port 3000...');
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import {z} from "zod";
+import {string, z} from "zod";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
+import { title } from "process";
 const app = express();
 const prisma = new PrismaClient();
 app.use(express.json());
@@ -48,8 +49,199 @@ app.post('/api/v1/signup', async(req,res)=>{
     }
 })
 
+const signInBody = z.object({
+    email:z.string().email(),
+    password:z.string()
+})
+app.post('/api/v1/signin', async(req,res)=>{
+    const {success} = signInBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            msg:'Wrong inputs'
+        })
+    }
+    try {
+        const user = await prisma.user.findUnique({
+            where:{
+                email:req.body.email
+            }
+        })
+        if(!user){
+            return res.status(411).json({
+                msg:'User does not exists'
+            })
+        }
+        const token = jwt.sign({userId:user.id},'babablacksheephavewool', {expiresIn:'1h'});
+        return res.json({
+            msg:`User with email: ${user.email} is logged in.`,
+            token:token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg:'Server Down'
+        })
+    }
+})
+
+
+
+
+const blogBody = z.object({
+    title:z.string(),
+    content:z.string()
+})
+interface DecodedToken {
+    userId: string;
+    iat: number;
+    exp: number;
+}
+app.post('/blog', async(req,res)=>{
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, 'babablacksheephavewool') as DecodedToken;
+        console.log('Decoded token:', decoded);
+        const {success} = blogBody.safeParse(req.body);
+        if(!success){
+            return res.status(411).json({
+                msg:'Wrong inputs'
+            })
+        }
+        const post = await prisma.post.create({
+            data:{
+                title:req.body.title,
+                content:req.body.content,
+                authorId:decoded.userId
+            }
+        })
+        if(!post){
+            return res.status(500).json({
+                msg: 'Server Down'
+            }); 
+        } 
+        return res.status(200).json({
+            msg:`blog post created having the title ${post.title}`
+        })
+    } catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+})
+
+//route to get a specific blog post
+app.get('/blog/:id', async(req,res)=>{
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, 'babablacksheephavewool') as DecodedToken;
+        console.log('Decoded token:', decoded);
+        const postId = req.params.id;
+        const post = await prisma.post.findUnique({
+            where:{
+                id:postId
+            }
+        })
+        if(!post){
+            return res.status(500).json({
+                msg: 'Server Down'
+            }); 
+        }
+        return res.json({
+            title:post.title,
+            content:post.content
+        })
+    } catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+})
+
+//route to get all the post that a user has
+
+app.get('/blogposts', async(req,res)=>{
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, 'babablacksheephavewool') as DecodedToken;
+        console.log('Decoded token:', decoded);
+
+        const posts = await prisma.post.findMany({
+            where:{
+                authorId:decoded.userId
+            }
+        })
+        
+        if(!posts){
+            return res.status(500).json({
+                msg: 'Server Down'
+            }); 
+        }
+        return res.json({
+            posts:posts
+        })
+    } catch (error) {
+        return res.status(401).json({
+            msg: 'Unauthorized. Invalid token.'
+        });
+    }
+})
+
+
+//route to update a blog post
+
+app.put('/blogupdate', async(req,res)=>{
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'Unauthorized. No token provided.'
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, 'babablacksheephavewool') as DecodedToken;
+        console.log('Decoded token:', decoded);
+        const title:string = req.body.title;
+        const content:string = req.body.content;
+        const id:string = req.body.id
+        const updatedPost = await prisma.post.update({
+            where:{
+                id:id,
+                authorId:decoded.userId
+            },
+            data:{
+                title:title,
+                content:content
+            }
+        })
+        return res.json({
+            updatedPost
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Server Down'
+        });
+    }
+})
+
 
 app.listen(3000,()=>{
-    console.log('working');
+    console.log('server is listening on port 3000...');
     
 })
